82270a0be3ae7c672cdcd50207f381637aa93426
Matt Tang <mattt123@gmail.com>
2024-04-30 13:01:03 +1000

    all commits

---
+++ src/abstract.js
@@ -0,0 +1,119 @@
+import data from "/src/assets/colors.json";
+const color_palette = data[0];
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const remakeButton = document.getElementById("start");
+const blurSlider = document.getElementById("BlurSlider");
+let colors;
+
+let which_col =Math.floor(Math.random() * 4345)
+
+    remakeButton.onclick = function () {
+    which_col = Math.floor(Math.random() * 4345)
+        drawImage();
+}
+let blurFactor = 4;
+blurSlider.oninput= function()
+{
+    blurFactor=this.value;
+    drawImage()
+}
+canvas.width = window.innerWidth * .8;
+canvas.height = window.innerHeight * .7;
+
+const n_boxes = 150;
+
+
+class Box {
+    constructor() {
+        this.x =-100+ Math.random() * canvas.width;
+        this.y =-100+ Math.random() * canvas.height;
+        this.w = 20 + Math.random() * 300;
+        this.h = 20 + Math.random() * 300;
+        let hexColor = colors[Math.floor(Math.random() * colors.length)];
+        let c = hex2rgb(hexColor);
+        this.color = `rgba(${c.r},${c.g},${c.b},1)`;
+    }
+
+    display() {
+        if (Math.random()>0.5)
+        {
+            ctx.globalCompositeOperation ='hard-light';
+        }
+        else
+        {
+            ctx.globalCompositeOperation ='darken';
+
+        }
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.w, this.h)
+    }
+
+
+}
+const hex2rgb = (hex) => {
+    const r = parseInt(hex.slice(1, 3), 16);
+    const g = parseInt(hex.slice(3, 5), 16);
+    const b = parseInt(hex.slice(5, 7), 16);
+    return { r, g, b };
+}
+let r, g, b, a;
+function createPixelNoise()
+{
+    let imageData = ctx.getImageData(0,0,canvas.width, canvas.height);
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        // Modify pixel data
+        if (Math.random() > .5)
+        {
+
+
+            r = 255;
+            g = 255;
+            b = 255;
+            a = 15;
+        } else {
+            r = 0;
+            g = 0;
+            b = 0;
+            a = 15;
+        }
+        imageData[i ++]*=100;
+        // g = data[i ++];
+        // b = data[i ++];
+        // a = data[i ++];
+    }
+
+
+    ctx.putImageData(imageData, 0, 0);
+
+}
+function applyBlur()
+{
+    const blurredRect = {x: 0, y: 0, width: canvas.width, height: canvas.height, spread: blurFactor};
+    //
+    ctx.filter = 'blur(' + blurredRect.spread + 'px)';
+    ctx.drawImage(canvas,
+        blurredRect.x, blurredRect.y, blurredRect.width, blurredRect.height,
+        blurredRect.x, blurredRect.y, blurredRect.width, blurredRect.height
+    );
+    createPixelNoise()
+
+}
+function drawImage() {
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    colors = color_palette[which_col];
+    console.log(color_palette.length)
+    for (let i = 0; i < n_boxes; i++) {
+
+        let box = new Box();
+        box.display(ctx);
+    }
+
+    applyBlur();
+}
+
+
+drawImage();
---
+++ src/static_flow_field.js
@@ -0,0 +1,110 @@
+import data from "/src/assets/colors.json";
+const color_palette = data[0];
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+
+let img = new Image();
+img.src = '/src/assets/top-20-small-dog-breeds.jpeg';
+// img.crossOrigin = "anonymous";
+canvas.height = window.innerHeight*.8;
+canvas.width = window.innerWidth*.9;
+
+import {createNoise3D,createNoise2D} from 'simplex-noise';
+const noise = createNoise2D();
+// const noise2 = createNoise3D();
+
+const steps=1000000;
+let current_x = Math.floor(canvas.width/2)
+let current_y = Math.floor(canvas.height/2);
+ctx.clearRect(0,0,canvas.width,canvas.height);
+const scl =0.002;
+ctx.strokeStyle=`rgba(0,0,0,0.1)`;
+let currentStep = 0;
+for (let i=0; i<steps; i++)
+{
+    // let dir = Math.random()*Math.PI;/
+    // let v = (3+noise(current_x*scl,current_y*scl,0))*40;
+    // dir+=.02;
+    let dir = noise(current_x*scl,current_y*scl)*Math.PI;
+    // let colorAngle = noise2(current_x*scl*.1,current_y*scl*.1,0)*Math.PI*2;
+    // ctx.strokeStyle=`hsl(${colorAngle*180/Math.PI},75%,50%)`;
+    let v =Math.random()*15;
+    let dx = v * Math.cos(dir);
+    let dy = v * Math.sin(dir);
+    let next_x = current_x+dx;
+    let next_y =current_y+dy
+    if (current_x<0 || current_x>=canvas.width || current_y<0 || current_y>=canvas.height ||currentStep>300)
+    {
+
+         next_x = Math.random()*canvas.width;
+         next_y = Math.random()*canvas.height;
+        current_y = next_y;
+        current_x = next_x;
+        currentStep=0;
+        // dir+=Math.PI/24;
+
+    }
+    currentStep++;
+    ctx.beginPath();
+    ctx.moveTo(current_x, current_y);
+    // ctx.arc(current_x,current_y,3,0,Math.PI*2);
+    ctx.lineTo(next_x, next_y);
+    ctx.stroke();
+    ctx.lineWidth=1;
+    current_x= next_x;
+    current_y= next_y;
+}
+
+
+//
+// img.onload = function() {
+//     canvas.width=this.width;
+//     canvas.height=this.height;
+//     ctx.drawImage(this,0,0)
+//     // for (let i =0; i<img.data)
+//     //
+//     const imageData = ctx.getImageData(0, 0, this.width, this.height); // get the image array
+//     let blank=[]
+//     let max =0;
+//     for  (let i =0; i<imageData.data.length;i+=4)
+//     {
+//         let sum =0 ;
+//         sum+= imageData.data[i]+imageData.data[i+1]+imageData.data[i+2];
+//         blank.push(sum);
+//         if (sum>max)
+//         {
+//             max=sum;
+//         }
+//     }
+//     const steps=1000;
+//     let current_x = Math.floor(this.width/2)
+//     let current_y = Math.floor(this.height/2);
+//     ctx.clearRect(0,0,this.width,this.height);
+//     ctx.strokeStyle='black';
+//     for (let i=0; i<steps; i++)
+//     {
+//         let dir = Math.random()*Math.PI;
+//         let idx = current_x+current_y*this.width;
+//         let v =(blank[idx]/max)*20;
+//         console.log(v);
+//         let dx = v * Math.cos(dir);
+//         let dy = v * Math.sin(dir);
+//         let next_x = Math.floor(current_x+dx);
+//         let next_y = Math.floor(current_y+dy)
+//         if (next_x<0 || next_x>=this.width || next_y<0 || next_y>=this.height)
+//         {
+//             break;
+//         }
+//         ctx.beginPath();
+//         ctx.moveTo(current_x, current_y);
+//         ctx.lineTo(next_x, next_y);
+//         ctx.stroke();
+//         ctx.lineWidth=1;
+//         current_x= next_x;
+//         current_y= next_y;
+//     }
+//
+// }
+//
+// // img.
